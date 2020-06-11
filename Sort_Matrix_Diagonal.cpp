b7bd470776d324863e1854e061da6491dbbc5e67
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-06-10 23:41:33 -0400

    Update Sort_Matrix_Diagonal.cpp

--- Sort_Matrix_Diagonal.cpp
+++ Sort_Matrix_Diagonal.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-
+        
+        /*
+           matrix entries in the same diagonal has equal value of i-j
+        */
+        
         int m = mat.size();
         int n = mat[0].size();
         unordered_map<int, vector<int>> table;
@@ -10,6 +14,10 @@ public:
                         table[i-j].push_back(mat[i][j]);
                 }
             }
+        
+        /*
+            sort vector in descending order using reverse iterator rbegin() and rend();
+        */
         
         for(int i = -(n-1); i<m; i++){
             sort(table[i].rbegin(), table[i].rend());

60d1f9d15b23a49df5561e1c08bbf9b24bc4f4dc
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-06-12 02:11:31 -0400

    Create Matrix_Rotation.cpp

---
+++ Matrix_Rotation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int N=matrix.size();
+        for(int x=0; x<N/2; x++){
+            for(int y=x; y<N-x-1; y++){
+                int tmp=matrix[x][y];
+                matrix[x][y]=matrix[N-y-1][x];
+                matrix[N-y-1][x]=matrix[N-x-1][N-y-1];
+                matrix[N-x-1][N-y-1]=matrix[y][N-x-1];
+                matrix[y][N-x-1]=tmp;
+            }
+        }
+    }
+};

8691e35121117ca1fdb1ce57f9ae88a8f668040a
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-06-15 21:36:50 -0400

    Update Two_Pointers.cpp

--- Two_Pointers.cpp
+++ Two_Pointers.cpp
@@ -19,3 +19,19 @@ static char __ = []() -> char {
     std::cout.tie(NULL);
     return '\0';
 }();
+
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};

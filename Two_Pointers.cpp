a5aa0c2173d4db9677b96ff5d397ac88da555673
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-06-15 21:24:04 -0400

    Create Two_Pointers.cpp

---
+++ Two_Pointers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k=0;
+        
+        for(int i=0;i<nums.size(); i++){
+            if(i==0||nums[i]!=nums[k-1]){   
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
+
+static char __ = []() -> char {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return '\0';
+}();

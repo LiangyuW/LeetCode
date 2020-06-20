343d1ee62dec2615856fc21ed825b9f33f6d8bb8
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-06-20 02:47:13 -0400

    Update DP_Minimum_Coin_Change.cpp

--- DP_Minimum_Coin_Change.cpp
+++ DP_Minimum_Coin_Change.cpp
@@ -15,3 +15,30 @@ public:
             return min==INT_MAX? -1:min;
         }
 };
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int f[amount+1];
+        f[0]=0;
+        
+        for(int i=1; i<=amount; i++){
+            f[i]=INT_MAX;
+        }
+        
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<coins.size(); j++){
+                if(coins[j]<=i){
+                    int subsum=f[i-coins[j]];
+                    if(subsum!=INT_MAX && subsum+1<f[i]){
+                        f[i]=subsum+1;
+                    }
+                }
+            }
+        }
+        
+        return f[amount]==INT_MAX?-1:f[amount];
+        
+    }
+};

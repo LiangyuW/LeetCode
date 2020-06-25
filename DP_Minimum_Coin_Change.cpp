d137ae2a9e3cd26a11df540cb3a4b7b2b902b25e
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-06-24 22:19:32 -0400

    Update DP_Minimum_Coin_Change.cpp

--- DP_Minimum_Coin_Change.cpp
+++ DP_Minimum_Coin_Change.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-            if(amount==0) return 0;
-            int min=INT_MAX;
+        vector<int> dp(amount+1, INT_MAX);
+        dp[0]=0;
         
-            for(int i=0; i<coins.size(); i++){
-                if(coins[i]<=amount){
-                    int submin=coinChange(coins, amount-coins[i]); 
-                    if(submin!=INT_MAX&&submin!=-1&&submin+1<min){
-                        min=submin+1;
-                    }
-                }
-            }
-            return min==INT_MAX? -1:min;
-        }
-};
-
-
-class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        int f[amount+1];
-        f[0]=0;
-        
-        for(int i=1; i<=amount; i++){
-            f[i]=INT_MAX;
-        }
-        
-        for(int i=1; i<=amount; i++){
-            for(int j=0; j<coins.size(); j++){
-                if(coins[j]<=i){
-                    int subsum=f[i-coins[j]];
-                    if(subsum!=INT_MAX && subsum+1<f[i]){
-                        f[i]=subsum+1;
-                    }
+        for(int coin: coins){
+            for(int i=coin; i<=amount; i++){
+                if(dp[i-coin]!=INT_MAX){
+                    dp[i]=std::min(dp[i], dp[i-coin]+1);
                 }
             }
         }
         
-        return f[amount]==INT_MAX?-1:f[amount];
+        return dp[amount]==INT_MAX?-1:dp[amount];
         
     }
 };
+
+static char __ = []() -> char {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return '\0';
+}();
+

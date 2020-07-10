aad9e4a961305a03ee3c7e104aea1a43ef1b467d
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-07-10 12:38:14 -0400

    Create Trie.cpp

---
+++ Trie/Trie.cpp
@@ -0,0 +1,78 @@
+class trienode{
+    public:
+    bool isleaf;
+    vector<trienode*> letters;
+    
+    trienode():
+        letters(26)
+    {
+        this->isleaf=false;
+        
+    }
+    
+    bool hasKey(char c){
+        return letters[c-'a']!=NULL;
+    }
+    
+    trienode* get(char c){
+        return letters[c-'a'];
+    }
+    
+    void insert(char c, trienode*n){
+        letters[c-'a']=n;
+    }
+    
+};
+
+
+
+class Trie {
+public:
+    trienode* root;
+    
+    Trie(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        root= new trienode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        trienode* n=root;
+        for(int i=0; i<word.size(); i++){
+            char c=word[i];
+            if(!n->hasKey(c)){
+                trienode*newnode=new trienode();
+                n->insert(c, newnode);
+            }
+            n=n->get(c);
+        }
+        n->isleaf=true;
+    }
+    
+    trienode* searchPrefix(string word){
+        trienode*n=root;
+        for(int i=0; i<word.size(); i++){
+            char c=word[i];
+            if(n->hasKey(c)){
+                n=n->get(c);
+            } else {
+                return NULL;
+            }
+        }
+        return n;
+    }
+    
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        trienode*n=searchPrefix(word);
+        return n!=NULL && n->isleaf;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        trienode*n=searchPrefix(prefix);
+        return n!=NULL;
+    }
+};

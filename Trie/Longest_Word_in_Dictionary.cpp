bf9a549ec691329f9c04febec52e5076d143765a
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-07-11 17:00:35 -0400

    Update Longest_Word_in_Dictionary.cpp

--- Trie/Longest_Word_in_Dictionary.cpp
+++ Trie/Longest_Word_in_Dictionary.cpp
@@ -30,3 +30,106 @@ public:
         return "";
     }
 };
+
+
+class trienode{
+    public:
+    bool isleaf;
+    string word;
+    vector<trienode*> letters;
+    
+    trienode():
+        letters(26)
+    {
+        this->isleaf=false;
+        
+    }    
+};
+
+
+class Trie {
+public:
+    trienode* root;
+    
+    Trie(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        root= new trienode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        trienode* n=root;
+        for(int i=0; i<word.size(); i++){
+            int index=word[i]-'a';
+            if(!n->letters[index]){
+                n->letters[index]=new trienode();
+            }
+            n=n->letters[index];
+        }
+        n->isleaf=true;
+        n->word=word;
+    }
+    
+    trienode* searchPrefix(string word){
+        trienode*n=root;
+        for(int i=0; i<word.size(); i++){
+            int index=word[i]-'a';
+            if(n->letters[index]){
+                n=n->letters[index];
+            } else {
+                return NULL;
+            }
+        }
+        return n;
+    }
+    
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        trienode*n=searchPrefix(word);
+        return n!=NULL && n->isleaf;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        trienode*n=searchPrefix(prefix);
+        return n!=NULL;
+    }
+};
+
+
+
+class Solution {
+public:
+    string longest=""; 
+    
+    string longestWord(vector<string>& words) {
+        Trie* tree= new Trie();
+        
+        for(auto e: words){
+            tree->insert(e);
+        }
+        dfs(tree->root);
+        return longest;
+    }
+    
+    void dfs(trienode* root){
+        if(root==nullptr) return;
+        
+        if(root->word!=""){
+            if(root->word.size()>longest.size()){
+                longest=root->word;
+            } else if (root->word.size()==longest.size() && root->word < longest){
+                longest=root->word;
+            }
+        }
+         
+        for(auto e: root->letters){
+            if(e && e->word!="") dfs(e);
+        }
+       
+    }
+    
+    
+};

471572e569b6864fd106ddc10a0619535b7c69ef
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-07-12 00:34:19 -0400

    Update prefix_sum.cpp

--- Math/prefix_sum.cpp
+++ Math/prefix_sum.cpp
@@ -2,6 +2,37 @@ class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         int N=1000000007;
+        
+        vector<int>sums;
+
+ 
+        for(int i = 0;i < n;i++) {
+            int sum = 0;
+            for (int j = i;j < n;j++) {
+                sum += nums[j];
+                sums.push_back(sum);
+            }
+           
+        }
+        
+        sort(sums.begin(), sums.end());
+        
+        int sum=0; 
+        for(int i=left-1; i<=right-1; i++){
+            sum=(sum+sums[i])%N;
+        }
+        
+        return sum;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        int N=1000000007;
         vector<int>prefixsums;
         vector<int>sums;
         prefixsums.push_back(0);

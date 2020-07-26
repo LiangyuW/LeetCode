fe02ef0bb4060dac984b9687391a9c28397be9dc
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-07-26 02:10:42 -0400

    Create 3sum.cpp

---
+++ 3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+         
+        vector<vector<int>>out;
+        sort(nums.begin(), nums.end());
+        int n=nums.size();
+        
+        for(int i=0; i<n-1; i++){
+            if (i >0 && nums[i-1]==nums[i]) continue;
+            int l=i+1;
+            int r=n-1;
+            int x=nums[i];
+            while(l<r){
+                int sum=nums[i] + nums[l] + nums[r];
+                if(sum<0 || l>i+1 && nums[l]==nums[l-1]){
+                    l++;
+                }
+                else if(sum>0 || r<n-1 && nums[r]==nums[r+1]){
+                    r--;
+                }
+                else {
+                    out.push_back({x, nums[l], nums[r]});
+                    l++;
+                    r--;
+                }
+            }
+            
+        }
+        return out;
+    }
+};

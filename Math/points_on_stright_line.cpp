f061201cd75a3d4e889c5ec0a436ad5c0689da43
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-07-29 14:20:51 -0400

    Update points_on_stright_line.cpp

--- Math/points_on_stright_line.cpp
+++ Math/points_on_stright_line.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        if(coordinates.size()<2) return false;
-        
+        int n=coordinates.size();
+        if(n<2) return false;
+        if(n==2) return true;
         int dy=coordinates[1][1]-coordinates[0][1];
         int dx=coordinates[1][0]-coordinates[0][0];
         
-        for(int i=2; i<coordinates.size(); i++){
+        for(int i=2; i<n; i++){
             int dy2=coordinates[i][1]-coordinates[1][1];
             int dx2=coordinates[i][0]-coordinates[1][0];
             if(dy*dx2!=dy2*dx){
@@ -17,3 +18,13 @@ public:
         return true;
     }
 };
+
+static char __ = []() -> char {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    return '\0';
+}();
+      
+      
+        

d41b71d85f28436747e2fa4a6c2ecc5733d3cf54
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-09-13 23:23:29 -0400

    Update similar_sentence.cpp

--- graph/similar_sentence.cpp
+++ graph/similar_sentence.cpp
@@ -1,3 +1,44 @@
+
+class Solution {
+public:
+    unordered_map<string, string>uf;
+    
+    
+    bool areSentencesSimilarTwo(vector<string>& words1, vector<string>& words2, vector<vector<string>>& pairs) {
+ 
+        if(words1.size()!=words2.size()) return false;
+            
+        for(auto &e: pairs){
+            merge(e[0],e[1]); 
+        }
+        
+        for(int i=0; i<words1.size(); i++){
+            if(find(words1[i])!=find(words2[i])) return false;
+        }
+        
+        return true;
+    }
+    
+    void merge(string x, string y){
+        x=find(x);
+        y=find(y);
+        if(x==y) return;
+        uf[x]=y;
+    }
+    
+    
+    string find(string s){
+        if(!uf.count(s))uf[s]=s;
+        if(uf[s]==s){
+            return s;
+        }
+        return uf[s]=find(uf[s]);
+    }
+};
+
+
+
+
 class Solution {
 public:
     bool areSentencesSimilarTwo(vector<string>& words1, vector<string>& words2, vector<vector<string>>& pairs) {

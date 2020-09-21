09b779abf16bd1a194b9c80933abcebda229dfda
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-09-21 02:21:00 -0400

    Create Lonely_Pixel.cpp

---
+++ Matrix/Lonely_Pixel.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findLonelyPixel(vector<vector<char>>& picture) {
+        int n=picture.size();
+        int m=picture[0].size();
+        vector<int>row(n, 0);
+        vector<int>col(m, 0);
+        int pixels=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                   if(picture[i][j]=='B'){
+                       row[i]++;
+                       col[j]++;
+                   }
+            } 
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(picture[i][j]=='B' && row[i]==1 && col[j]==1) pixels++;
+            }
+        }
+        
+        return pixels;
+    }
+};
